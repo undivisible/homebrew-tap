6911a75e163b72d87d4a6532ebc321f59e8bb9c4
max lee carter <undivisible@vk.com>
2026-04-01 20:00:29 +1100

    fix: make poke-around formula build without bun

--- Formula/poke-around.rb
+++ Formula/poke-around.rb
@@ -4,13 +4,10 @@ class PokeAround < Formula
   license "MPL-2.0"
   head "https://github.com/undivisible/poke-around.git", branch: "main"
 
-  depends_on "bun" => :build
   depends_on "zig" => :build
   depends_on "node"
 
   def install
-    system "bun", "install", "--frozen-lockfile", chdir: "bridge"
-    system "bun", "run", "build:bridge"
     system "zig", "build", *std_zig_args
     cp_r "bridge/dist/traybin", bin
   end
