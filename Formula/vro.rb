13c51d9fc86032b47cd0d6e6b6450222d0dd8ce8
undivisible <undivisible@vk.com>
2026-05-24 13:45:44 +1000

    vro: refresh v1.0.5 checksums

--- Formula/vro.rb
+++ Formula/vro.rb
@@ -7,18 +7,18 @@ class Vro < Formula
   on_macos do
     on_arm do
       url "https://github.com/undivisible/vro/releases/download/v1.0.5/vro-macos-aarch64.tar.gz"
-      sha256 "b1e54533d6d9b098656b78047edf6968c93743ea12d4fd5910131551cd6f8e7a"
+      sha256 "e3e142a3352452cda61041b7fa50db28469ff4355c02309282cb624f0c035760"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/undivisible/vro/releases/download/v1.0.5/vro-linux-x86_64.tar.gz"
-      sha256 "7836ff8eee438052a674ddd6e20fd6a4d62b9f0f7663fd5a7ec6245f1275d213"
+      sha256 "ed0f91f2efe30388a236997ed23faede2d3f9de0a98f9f3598bd8ce0bfbe3d9f"
     end
     on_arm do
       url "https://github.com/undivisible/vro/releases/download/v1.0.5/vro-linux-aarch64.tar.gz"
-      sha256 "cb57f0750fbfc07dbfa65d7284d984901df1aab901c5bac348c1e517566bb004"
+      sha256 "3a23db0c926446f911543a1983a8c5282e9382b11d272985f889e87708db94aa"
     end
   end
 
