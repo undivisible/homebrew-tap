f29f3b2c5b70e7c4eb4d69bfb5e606c28fbad3a6
undivisible <undivisible@vk.com>
2026-06-08 01:23:18 +1000

    poke-around 0.4.0

--- Formula/poke-around.rb
+++ Formula/poke-around.rb
@@ -2,35 +2,32 @@ class PokeAround < Formula
   desc "Expose your machine to your Poke AI assistant via an MCP tunnel"
   homepage "https://github.com/undivisible/poke-around"
   license "MPL-2.0"
-  version "0.3.20"
+  version "0.4.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.3.20/poke-around-macos-aarch64.tar.gz"
-      sha256 "1c144dac96351aadfbb318b2ec1f7946105785357814f2a408b7c134a1da316d"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.4.0/poke-around-macos-aarch64.tar.gz"
+      sha256 "12e5768e83dc4ae315b3a0cc1b801ab42b04e5defcacbfaf11911cb1eb9c930e"
     end
     on_intel do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.3.20/poke-around-macos-x86_64.tar.gz"
-      sha256 "dbd7ab1fb9f5ee80f60b119f3f5e28b1bd09431cd3c8fee975128e631953866b"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.4.0/poke-around-macos-x86_64.tar.gz"
+      sha256 "acb4214898b99c51fb6bda17480d43630974a0e119dd51793448a0d725675302"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.3.20/poke-around-linux-aarch64.tar.gz"
-      sha256 "59e345330bc6025283a30b92a590b290613e9873d106b4994d38dc20c926eab9"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.4.0/poke-around-linux-aarch64.tar.gz"
+      sha256 "7965ea0979f88d5f8817d21aca84c6b06a19ae2acc6ac62698c9cdf5faedecdc"
     end
     on_intel do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.3.20/poke-around-linux-x86_64.tar.gz"
-      sha256 "b6114ed5a3cfb72d11073e4f7a17f234b7e58cd25cbfb8b924d9ee4fe75d6ad2"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.4.0/poke-around-linux-x86_64.tar.gz"
+      sha256 "533085a2b0f9b69c46fdf84c41b2e21a8fca33c7c09c4a5780d23e028b11459a"
     end
   end
 
-  depends_on "node"
-
   def install
     bin.install "poke-around"
-    bin.install "poke-around-bridge.js"
   end
 
   test do
