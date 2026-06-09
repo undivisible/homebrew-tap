8eb9488e0594bf79dfd09cdf96fe71ecfb4e9990
Max Carter 祁明思 <undivisible@vk.com>
2026-06-09 13:53:23 +1000

    vro 1.1.1
    Fixes command bar backspace handling.

--- Formula/vro.rb
+++ Formula/vro.rb
@@ -2,23 +2,23 @@ class Vro < Formula
   desc "Minimal terminal text editor (micro-inspired)"
   homepage "https://github.com/undivisible/vro"
   license "MPL-2.0"
-  version "1.1.0"
+  version "1.1.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/undivisible/vro/releases/download/v1.1.0/vro-macos-aarch64.tar.gz"
-      sha256 "6e39ecbf35670cf8b5d0f76322dddfdd28b049264a12d973dbb50131c947b1ed"
+      url "https://github.com/undivisible/vro/releases/download/v1.1.1/vro-macos-aarch64.tar.gz"
+      sha256 "8c4273789922092fa6a1b69f83d73d0bd1dadbef18f0e8336646721f90c214f5"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/undivisible/vro/releases/download/v1.1.0/vro-linux-x86_64.tar.gz"
-      sha256 "215da5872394e981e359a9880a12d68d81f60d03a5ca4e819bf163411b0fb642"
+      url "https://github.com/undivisible/vro/releases/download/v1.1.1/vro-linux-x86_64.tar.gz"
+      sha256 "1a89423ce37b80e7c67a1c81af73f61de70380f3966fd9da64c35317e1871f74"
     end
     on_arm do
-      url "https://github.com/undivisible/vro/releases/download/v1.1.0/vro-linux-aarch64.tar.gz"
-      sha256 "2c99caa8fd1ed3858c0285f776103a5a07389eceac94f4d06bdc8a7482c74999"
+      url "https://github.com/undivisible/vro/releases/download/v1.1.1/vro-linux-aarch64.tar.gz"
+      sha256 "4afe0674eb7c36999a43b4ec1ada35e27c8ec204521d28105f1d3300ac283bfe"
     end
   end
 
