68b0a0cdb5d900e6815656094cf26a2df872c8cd
Max Carter 祁明思 <undivisible@vk.com>
2026-06-10 10:36:11 +1000

    Bump poke-around to v0.5.9

--- Formula/poke-around.rb
+++ Formula/poke-around.rb
@@ -1,28 +1,28 @@
 class PokeAround < Formula
   desc "Expose your machine to your Poke AI assistant via an MCP tunnel"
   homepage "https://github.com/undivisible/poke-around"
-  version "0.5.8"
+  version "0.5.9"
   license "MPL-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.5.8/poke-around-macos-aarch64.tar.gz"
-      sha256 "af6f78b94992b7312a7789722dc19db12acd533a20f236e459103066ed779ec4"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.5.9/poke-around-macos-aarch64.tar.gz"
+      sha256 "75b21cb4ef4cc0ad72bf85183afae529d9b8548afdb21b81a07de07135d640cd"
     end
     on_intel do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.5.8/poke-around-macos-x86_64.tar.gz"
-      sha256 "60a653e3775e9a13f369a32b7bb44d9a34fe4edf53e75a2c365265579249c3be"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.5.9/poke-around-macos-x86_64.tar.gz"
+      sha256 "2e23345ac5f93b8d5e22321435b2f4cea0248fe5626c016ea18d6cf2c0a49a75"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.5.8/poke-around-linux-aarch64.tar.gz"
-      sha256 "5229576637a32bfdc548f936128c35c2056e5bbf0acffc06f1bef87f28d9cf91"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.5.9/poke-around-linux-aarch64.tar.gz"
+      sha256 "8f47ce26c7552eb346052effa5fa20849959b95989d85c19874e8e25181bb2d7"
     end
     on_intel do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.5.8/poke-around-linux-x86_64.tar.gz"
-      sha256 "3a2038df7875ac7f070f3f372088f6fdd4e6c42dcc189d00fe4996832d10447c"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.5.9/poke-around-linux-x86_64.tar.gz"
+      sha256 "92a7da84d9156b32c5574eee0007781d9fe5189fcba7d10d531767d81987b958"
     end
   end
 
