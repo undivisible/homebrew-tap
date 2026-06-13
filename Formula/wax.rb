81688382058c072763b53b6d3040ae5e34e9bec2
Max Carter 祁明思 <undivisible@vk.com>
2026-06-13 12:19:19 +1000

    wax 0.17.14

--- Formula/wax.rb
+++ Formula/wax.rb
@@ -1,8 +1,8 @@
 class Wax < Formula
   desc "Fast Homebrew-compatible package manager"
   homepage "https://github.com/tschk/wax"
-  url "https://github.com/tschk/wax/archive/refs/tags/v0.17.13.tar.gz"
-  sha256 "f07b189bc40b77536010dfb012abdb31fa9d8080219fa1eed0cf730e51645892"
+  url "https://github.com/tschk/wax/archive/refs/tags/v0.17.14.tar.gz"
+  sha256 "587e86c7b0eddd878849caf6cb5872ddf3201e009e4b8e5813bea605ae6ee78c"
   head "https://github.com/tschk/wax.git", branch: "master"
   license "MIT"
 
