cf01ad017cd747c829e697097e525d1c417ec370
Max Carter 祁明思 <undivisible@vk.com>
2026-06-15 17:28:13 +1000

    vro: simplify to binary release only (--head needs separate formula)

--- Formula/vro.rb
+++ Formula/vro.rb
@@ -4,22 +4,15 @@ class Vro < Formula
   license "MPL-2.0"
   version "1.1.1"
 
-  on_linux do
-    url "https://github.com/undivisible/vro/releases/download/v1.1.1/vro-linux-x86_64.tar.gz"
-    sha256 "1a89423ce37b80e7c67a1c81af73f61de70380f3966fd9da64c35317e1871f74"
-  end
+  url "https://github.com/undivisible/vro/releases/download/v1.1.1/vro-linux-x86_64.tar.gz"
+  sha256 "1a89423ce37b80e7c67a1c81af73f61de70380f3966fd9da64c35317e1871f74"
 
   head "https://github.com/undivisible/vro.git", branch: "main"
 
   depends_on "vlang" => :build
 
   def install
-    if build.head?
-      system "v", "-prod", "main.v"
-      bin.install "main" => "vro"
-    else
-      bin.install "vro"
-    end
+    bin.install "vro"
     (share/"vro/syntax").install Dir["syntax/*"]
   end
 
