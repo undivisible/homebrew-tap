cd39577bd714071704387b648a8c66658b4b2a04
Max Carter 祁明思 <undivisible@vk.com>
2026-06-15 16:36:20 +1000

    add oil v0.2.0, remove wax (renamed to oil)

---
+++ Formula/oil.rb
@@ -0,0 +1,18 @@
+class Oil < Formula
+  desc "Native Linux package manager with system-PM interop"
+  homepage "https://github.com/semitechnological/oil"
+  url "https://github.com/semitechnological/oil/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "8c26759c2435149cdeee716c312f833f5f174b84c83eec8944dcf7bfe9fc2502"
+  head "https://github.com/semitechnological/oil.git", branch: "master"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    assert_match "oil", shell_output("#{bin}/oil --version")
+  end
+end
