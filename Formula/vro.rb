cfa48e5510446b3d6553ab0825d3b263d725e9a0
Max Carter 祁明思 <undivisible@vk.com>
2026-06-15 17:21:09 +1000

    vro: binary release default, --head builds from source with V

--- Formula/vro.rb
+++ Formula/vro.rb
@@ -4,14 +4,23 @@ class Vro < Formula
   license "MPL-2.0"
   version "1.1.1"
 
-  url "https://github.com/undivisible/vro/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "8e6bf55f633fd0d8b182f4e137b5877ea6fec66e8436c29d433efee3e0fc09d5"
+  on_linux do
+    url "https://github.com/undivisible/vro/releases/download/v1.1.1/vro-linux-x86_64.tar.gz"
+    sha256 "1a89423ce37b80e7c67a1c81af73f61de70380f3966fd9da64c35317e1871f74"
+  end
+
   head "https://github.com/undivisible/vro.git", branch: "main"
 
-  depends_on "rust" => :build
+  depends_on "vlang" => :build
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    if build.head?
+      system "v", "-prod", "main.v"
+      bin.install "main" => "vro"
+    else
+      bin.install "vro"
+    end
+    (share/"vro/syntax").install Dir["syntax/*"]
   end
 
   test do
