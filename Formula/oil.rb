9a1643e202e9bb0af61a6c11def40825e340ae3c
Max Carter 祁明思 <undivisible@vk.com>
2026-06-15 17:10:44 +1000

    update oil v0.2.0 sha256

--- Formula/oil.rb
+++ Formula/oil.rb
@@ -2,7 +2,7 @@ class Oil < Formula
   desc "Native Linux package manager with system-PM interop"
   homepage "https://github.com/semitechnological/oil"
   url "https://github.com/semitechnological/oil/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "ad78bc9eb1f8ff34c52ba6b6b0ca57746dbdf2030c171d5c340cfe5da70f0d6a"
+  sha256 "0c0f69a236c34fd9de18de8d12ac06b4904d197237d7c021e30b8aa97a5a3d1c"
   head "https://github.com/semitechnological/oil.git", branch: "master"
   license "MIT"
 
