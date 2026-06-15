176c3d4dd0c048d24523be0b3b4a6570e7b63d92
Max Carter 祁明思 <undivisible@vk.com>
2026-06-15 16:59:15 +1000

    vro: add source build (cargo install), works on musl

--- Formula/vro.rb
+++ Formula/vro.rb
@@ -4,27 +4,14 @@ class Vro < Formula
   license "MPL-2.0"
   version "1.1.1"
 
-  on_macos do
-    on_arm do
-      url "https://github.com/undivisible/vro/releases/download/v1.1.1/vro-macos-aarch64.tar.gz"
-      sha256 "8c4273789922092fa6a1b69f83d73d0bd1dadbef18f0e8336646721f90c214f5"
-    end
-  end
+  url "https://github.com/undivisible/vro/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "8e6bf55f633fd0d8b182f4e137b5877ea6fec66e8436c29d433efee3e0fc09d5"
+  head "https://github.com/undivisible/vro.git", branch: "main"
 
-  on_linux do
-    on_intel do
-      url "https://github.com/undivisible/vro/releases/download/v1.1.1/vro-linux-x86_64.tar.gz"
-      sha256 "1a89423ce37b80e7c67a1c81af73f61de70380f3966fd9da64c35317e1871f74"
-    end
-    on_arm do
-      url "https://github.com/undivisible/vro/releases/download/v1.1.1/vro-linux-aarch64.tar.gz"
-      sha256 "4afe0674eb7c36999a43b4ec1ada35e27c8ec204521d28105f1d3300ac283bfe"
-    end
-  end
+  depends_on "rust" => :build
 
   def install
-    bin.install "vro"
-    (share/"vro/syntax").install Dir["syntax/*"]
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
