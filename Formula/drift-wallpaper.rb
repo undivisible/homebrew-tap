a7969ab3aa6d0a7e080af0271fe783221087fc26
Max Carter 祁明思 <undivisible@vk.com>
2026-06-23 19:24:03 +1000

    drift-wallpaper 0.1.43 (embed settings UI)

--- Formula/drift-wallpaper.rb
+++ Formula/drift-wallpaper.rb
@@ -1,22 +1,22 @@
 class DriftWallpaper < Formula
   desc "Fluid live wallpaper for macOS, Windows, and Linux"
   homepage "https://github.com/undivisible/drift-wallpaper"
-  version "0.1.41"
+  version "0.1.43"
   head "https://github.com/undivisible/drift-wallpaper.git", branch: "m"
 
   # macOS – Apple Silicon (ARM64)
   on_macos do
     on_arm do
-      url "https://github.com/undivisible/drift-wallpaper/releases/download/v0.1.41/drift-wallpaper-macos-aarch64.tar.gz"
-      sha256 "8857ab475099fdf0d1d55596df819a7d0d70bb92a854169fd8017fbae449be61"
+      url "https://github.com/undivisible/drift-wallpaper/releases/download/v0.1.43/drift-wallpaper-macos-aarch64.tar.gz"
+      sha256 "0ef2845a9d947a2b35689155df567f930417cefdfb576d069ded958277e1b6bf"
     end
   end
 
   # Linux – x86_64 (amd64)
   on_linux do
     on_x86_64 do
-      url "https://github.com/undivisible/drift-wallpaper/releases/download/v0.1.41/drift-wallpaper-linux-x86_64.tar.gz"
-      sha256 "221758a724fe54229348a347d9776e81b0d69ff8c4f5c8beb1a25094ff2a0867"
+      url "https://github.com/undivisible/drift-wallpaper/releases/download/v0.1.43/drift-wallpaper-linux-x86_64.tar.gz"
+      sha256 "6c7a8b7f70c2f94b2bf5b7038904525689ee619bf52021ff580db1ee40346226"
     end
   end
 
