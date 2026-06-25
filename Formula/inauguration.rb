086a87372532bce3e267f24d9b05eb266a0d1ace
Max Carter 祁明思 <undivisible@vk.com>
2026-06-25 12:49:02 +1000

    inauguration: bump to v0.6.14

--- Formula/inauguration.rb
+++ Formula/inauguration.rb
@@ -1,20 +1,20 @@
 class Inauguration < Formula
   desc "Multi-language compiler with JIT, Core IR, and 40 frontends"
   homepage "https://github.com/tschk/inauguration"
-  version "0.6.7"
+  version "0.6.14"
   head "https://github.com/tschk/inauguration.git", branch: "master"
 
   on_macos do
     on_arm do
-      url "https://github.com/tschk/inauguration/releases/download/v0.6.7/in-macos-aarch64.tar.gz"
-      sha256 "placeholder"
+      url "https://github.com/tschk/inauguration/releases/download/v0.6.14/in-macos-aarch64.tar.gz"
+      sha256 "PLACEHOLDER_UPDATE_AFTER_CI_RELEASE"
     end
   end
 
   on_linux do
     on_x86_64 do
-      url "https://github.com/tschk/inauguration/releases/download/v0.6.7/in-linux-x86_64.tar.gz"
-      sha256 "placeholder"
+      url "https://github.com/tschk/inauguration/releases/download/v0.6.14/in-linux-x86_64.tar.gz"
+      sha256 "PLACEHOLDER_UPDATE_AFTER_CI_RELEASE"
     end
   end
 
