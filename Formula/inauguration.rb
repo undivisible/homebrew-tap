f4665abc2d8f4e386e6357935a542b199e0dc463
Max Carter 祁明思 <undivisible@vk.com>
2026-06-25 10:57:35 +1000

    inauguration: bump to v0.6.7 (JIT supports classes, floats, match)

--- Formula/inauguration.rb
+++ Formula/inauguration.rb
@@ -1,19 +1,19 @@
 class Inauguration < Formula
   desc "Multi-language compiler with JIT, Core IR, and 40 frontends"
   homepage "https://github.com/tschk/inauguration"
-  version "0.6.5"
+  version "0.6.7"
   head "https://github.com/tschk/inauguration.git", branch: "master"
 
   on_macos do
     on_arm do
-      url "https://github.com/tschk/inauguration/releases/download/v0.6.5/in-macos-aarch64.tar.gz"
+      url "https://github.com/tschk/inauguration/releases/download/v0.6.7/in-macos-aarch64.tar.gz"
       sha256 "placeholder"
     end
   end
 
   on_linux do
     on_x86_64 do
-      url "https://github.com/tschk/inauguration/releases/download/v0.6.5/in-linux-x86_64.tar.gz"
+      url "https://github.com/tschk/inauguration/releases/download/v0.6.7/in-linux-x86_64.tar.gz"
       sha256 "placeholder"
     end
   end
