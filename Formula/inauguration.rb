fc57cc26fe7eaf829c0b784b1146c384cb59a2cc
Max Carter 祁明思 <undivisible@vk.com>
2026-06-25 13:02:04 +1000

    inauguration: v0.6.14 SHA from local build

--- Formula/inauguration.rb
+++ Formula/inauguration.rb
@@ -7,14 +7,14 @@ class Inauguration < Formula
   on_macos do
     on_arm do
       url "https://github.com/tschk/inauguration/releases/download/v0.6.14/in-macos-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_UPDATE_AFTER_CI_RELEASE"
+      sha256 "2ebaf31b01170f7bbeb7bfca97676a3f26ee694edfabce47ec87c4034e3ec93b"
     end
   end
 
   on_linux do
     on_x86_64 do
       url "https://github.com/tschk/inauguration/releases/download/v0.6.14/in-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_UPDATE_AFTER_CI_RELEASE"
+      sha256 "2ebaf31b01170f7bbeb7bfca97676a3f26ee694edfabce47ec87c4034e3ec93b"
     end
   end
 
