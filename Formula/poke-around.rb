72e157c668a4d4e4ed5ade50828bd6213e1a293c
Max Carter 祁明思 <undivisible@vk.com>
2026-06-26 22:30:58 +1000

    brew: update poke-around to v0.6.1

--- Formula/poke-around.rb
+++ Formula/poke-around.rb
@@ -1,28 +1,28 @@
 class PokeAround < Formula
   desc "Expose your machine to your Poke AI assistant via an MCP tunnel"
   homepage "https://github.com/undivisible/poke-around"
-  version "0.6.0"
+  version "0.6.1"
   license "MPL-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.6.0/poke-around-macos-aarch64.tar.gz"
-      sha256 "93b88789a0bdc58a0c068a5af5424f1e2c655ce36520f3d0005b85e7d332d0f0"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.6.1/poke-around-macos-aarch64.tar.gz"
+      sha256 "cc45c7ec9857150fceac01a0311145ed098ff56a15f2c6004584bf39fb7217ca"
     end
     on_intel do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.6.0/poke-around-macos-x86_64.tar.gz"
-      sha256 "0b288c75513d3611bac82a3418b1708b029b25f19ff2d6528243d183e098302d"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.6.1/poke-around-macos-x86_64.tar.gz"
+      sha256 "f340b330372ccd2594df10c9bb4a3e723ef2c43a0ea93a19195d3f9f1bf623b6"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.6.0/poke-around-linux-aarch64.tar.gz"
-      sha256 "4f41b601a5a7697eef0e47a652815c47ac29798b4a1f04cf44391b1ca15a07c6"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.6.1/poke-around-linux-aarch64.tar.gz"
+      sha256 "f2c6ee5a58b56dcda9bb8784240ba5a5cb9e8f5dfb17f48657743a4e7dec0085"
     end
     on_intel do
-      url "https://github.com/undivisible/poke-around/releases/download/v0.6.0/poke-around-linux-x86_64.tar.gz"
-      sha256 "2de73a4fedb9720fc174e3533b281369d145ac5da95cc686538b0704771aa6c0"
+      url "https://github.com/undivisible/poke-around/releases/download/v0.6.1/poke-around-linux-x86_64.tar.gz"
+      sha256 "670bfd6a7bfa70565b442ae5756978e8cf92c30c09f3027f43f9e0c54753f1c6"
     end
   end
 
