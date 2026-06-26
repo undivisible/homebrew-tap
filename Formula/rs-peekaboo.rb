e1c8e58736f40acc4fbbdf496913581976c25900
Max Carter 祁明思 <undivisible@vk.com>
2026-06-26 21:55:39 +1000

    brew: update rs-peekaboo sha256 for v0.3.0

--- Formula/rs-peekaboo.rb
+++ Formula/rs-peekaboo.rb
@@ -2,7 +2,7 @@ class RsPeekaboo < Formula
   desc "Rust-native cross-platform computer-use CLI and library"
   homepage "https://github.com/undivisible/rs_peekaboo"
   url "https://github.com/undivisible/rs_peekaboo/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "9b4f0f712f4708f98cb3eb5d102f9899e30a8c674f4606953ed1c91cef9d2c0f"
   license "MPL-2.0"
   version "0.3.0"
 
