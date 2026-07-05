d2cd90a8b65af67c47e5e7c2a728464f9cf96d91
Max Carter 祁明思 <undivisible@vk.com>
2026-07-05 17:22:19 +1000

    herdr-gui: bump to 0.1.2 with static ghostty

--- Formula/herdr-gui.rb
+++ Formula/herdr-gui.rb
@@ -1,15 +1,15 @@
 class HerdrGui < Formula
   desc "macOS GUI for Herdr"
   homepage "https://github.com/undivisible/herdr-gui"
-  version "0.1.0"
+  version "0.1.2"
   license "MPL-2.0"
   head "https://github.com/undivisible/herdr-gui.git", branch: "master"
 
   # macOS – Apple Silicon (ARM64)
   on_macos do
     on_arm do
-      url "https://github.com/undivisible/herdr-gui/releases/download/v0.1.0/herdr-gui-macos-aarch64.tar.gz"
-      sha256 "63346e720f960b5c1ac698bec4fd712657bace1d8e2d07c7641a14ca495d5f00"
+      url "https://github.com/undivisible/herdr-gui/releases/download/v0.1.2/herdr-gui-macos-aarch64.tar.gz"
+      sha256 "a81ff369f85167e1e7633953e39fdfad3eaff9118d05bb902c32105818a6db38"
     end
   end
 
