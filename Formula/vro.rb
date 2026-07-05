434d4654bb8a42f466df1338f1c127527c49ade1
Max Carter 祁明思 <undivisible@vk.com>
2026-07-05 14:45:25 +1000

    Update vro formula to 1.2.2

--- Formula/vro.rb
+++ Formula/vro.rb
@@ -2,26 +2,26 @@ class Vro < Formula
   desc "Minimal terminal text editor (micro-inspired)"
   homepage "https://github.com/undivisible/vro"
   license "MPL-2.0"
-  version "1.2.1"
+  version "1.2.2"
   head "https://github.com/undivisible/vro.git", branch: "main"
 
   # macOS – Apple Silicon (ARM64)
   on_macos do
     on_arm do
-      url "https://github.com/undivisible/vro/releases/download/v1.2.1/vro-macos-aarch64.tar.gz"
-      sha256 "2250d91bbf19b9f56d145a96b17f43723a3dfc6037aec63909194af02c6a4bb2"
+      url "https://github.com/undivisible/vro/releases/download/v1.2.2/vro-macos-aarch64.tar.gz"
+      sha256 "910632272acb8f9a45702ccc8c96e259a438fb60191f077e177292174090da76"
     end
   end
 
   # Linux – x86_64 (amd64)
   on_linux do
     on_x86_64 do
-      url "https://github.com/undivisible/vro/releases/download/v1.2.1/vro-linux-x86_64.tar.gz"
-      sha256 "6ae5e86216a91ea64ab887a7dc087049b63d7326757b55f129729537bc8fe6a3"
+      url "https://github.com/undivisible/vro/releases/download/v1.2.2/vro-linux-x86_64.tar.gz"
+      sha256 "b6862272ebdebd23e44645bc34b4d444e6368b658acf74296e8e725eabd30459"
     end
     on_arm do
-      url "https://github.com/undivisible/vro/releases/download/v1.2.1/vro-linux-aarch64.tar.gz"
-      sha256 "3a95925965df219995f28005875d7dc27bd3e8db6bf371216bd8bfc37db8e218"
+      url "https://github.com/undivisible/vro/releases/download/v1.2.2/vro-linux-aarch64.tar.gz"
+      sha256 "73a26dfe6b04740908511dc463f06018d4b54714d095bda0cf6098f89aec1ad5"
     end
   end
 
