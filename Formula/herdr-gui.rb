abd87e6274216898d32db288cec4b20ee7b50f8c
Max Carter 祁明思 <undivisible@vk.com>
2026-07-10 13:17:35 +1000

    herdr-gui 0.1.4: fix sidebar headers, workspace git branch, spaces duplication

--- Formula/herdr-gui.rb
+++ Formula/herdr-gui.rb
@@ -1,15 +1,15 @@
 class HerdrGui < Formula
   desc "macOS GUI for Herdr"
   homepage "https://github.com/undivisible/herdr-gui"
-  version "0.1.3"
+  version "0.1.4"
   license "MPL-2.0"
   head "https://github.com/undivisible/herdr-gui.git", branch: "master"
 
   # macOS – Apple Silicon (ARM64)
   on_macos do
     on_arm do
-      url "https://github.com/undivisible/herdr-gui/releases/download/v0.1.3/herdr-gui-macos-aarch64.tar.gz"
-      sha256 "1517880430641825972685f44241b3c0d59e6c51efd6e7a1592b81e9bb2ff6b4"
+      url "https://github.com/undivisible/herdr-gui/releases/download/v0.1.4/herdr-gui-macos-aarch64.tar.gz"
+      sha256 "7533cbf5c46907db5e38d998466017580dea3d0c6e866f5620068c9949c88b32"
     end
   end
 
