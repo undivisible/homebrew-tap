525dfcc272b7663a5a06174ab99353e8824ece80
Max Carter 祁明思 <undivisible@vk.com>
2026-07-15 19:34:47 +1000

    herdr-gui 0.1.6

--- Formula/herdr-gui.rb
+++ Formula/herdr-gui.rb
@@ -1,15 +1,15 @@
 class HerdrGui < Formula
   desc "macOS GUI for Herdr"
   homepage "https://github.com/undivisible/herdr-gui"
-  version "0.1.5"
+  version "0.1.6"
   license "MPL-2.0"
   head "https://github.com/undivisible/herdr-gui.git", branch: "master"
 
   # macOS – Apple Silicon (ARM64)
   on_macos do
     on_arm do
-      url "https://github.com/undivisible/herdr-gui/releases/download/v0.1.5/herdr-gui-macos-aarch64.tar.gz"
-      sha256 "5a9ffd71a6a1e32df9efb53c28217091c51228b127961fa6e89909f3e2cffac7"
+      url "https://github.com/undivisible/herdr-gui/releases/download/v0.1.6/herdr-gui-macos-aarch64.tar.gz"
+      sha256 "d6966f8a8a37fa825ebd9e7de5a0a34dd3bd0b7b7217b43d2fe15e5a161c86a0"
     end
   end
 
